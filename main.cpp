0933a15fc5b88f991be8323618e9ec542e14f528
Hardik Patel <82847503+P-Hardik@users.noreply.github.com>
2023-01-06 16:02:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
     
     Here is an example of a user input for an incomplete Sudoku:
     
-    IMPORTANT: MAKE SURE THERE IS SPACE BETWEEN EACH BUMBER!!!
+    IMPORTANT: MAKE SURE THERE IS SPACE BETWEEN EACH NUMBER!!!
     3 0 6 5 0 8 4 0 0
     5 2 0 0 0 0 0 0 0
     0 8 7 0 0 0 0 3 1       
